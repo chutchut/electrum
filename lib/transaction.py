8793a0b89bf944ef395debd20ac5cb41d68810bd
Calum Hutton <c.e.hutton@gmx.com>
2018-01-29 13:06:52 +0000

    Comment debug output

--- lib/transaction.py
+++ lib/transaction.py
@@ -161,10 +161,10 @@ class TESDataStream(object):
     def write(self, _bytes):  # Initialize with string of _bytes
         if self.input is None:
             self.input = bytearray(_bytes)
-            print_msg("Set input for TESDataStream:", self.input)
+            # print_msg("Set input for TESDataStream:", self.input)
         else:
             self.input += bytearray(_bytes)
-            print_msg("Add to input for TESDataStream:", self.input)
+            # print_msg("Add to input for TESDataStream:", self.input)
 
     def read_string(self, encoding='ascii'):
         # Strings are encoded depending on length:
@@ -178,10 +178,10 @@ class TESDataStream(object):
             raise SerializationError("call write(bytes) before trying to deserialize")
 
         length = self.read_compact_size()
-        print_msg("read_string() length:", length)
+        # print_msg("read_string() length:", length)
 
         content = self.read_bytes(length).decode(encoding)
-        print_msg("read_string() result:", content)
+        # print_msg("read_string() result:", content)
 
         return content
 
@@ -193,10 +193,10 @@ class TESDataStream(object):
 
     def read_bytes(self, length):
         try:
-            self.report_buffer()
-            print_msg("read_bytes() length:", length)
+            # self.report_buffer()
+            # print_msg("read_bytes() length:", length)
             result = self.input[self.read_cursor:self.read_cursor+length]
-            print_msg("read_bytes() result:", result)
+            # print_msg("read_bytes() result:", result)
             self.read_cursor += length
             return result
         except IndexError:
@@ -249,10 +249,10 @@ class TESDataStream(object):
 
     def _read_num(self, format):
         try:
-            self.report_buffer()
-            print_msg("_read_num() format:", format)
+            # self.report_buffer()
+            # print_msg("_read_num() format:", format)
             (i,) = struct.unpack_from(format, self.input, self.read_cursor)
-            print_msg("_read_num() result:", i)
+            # print_msg("_read_num() result:", i)
             self.read_cursor += struct.calcsize(format)
         except Exception as e:
             raise SerializationError(e)
@@ -390,15 +390,15 @@ def decode_script(bytes):
 
 def match_decoded(decoded, to_match):
     if len(decoded) != len(to_match):
-        print_msg("match_decoded() list length mismatch (expected {}, got {})".format(len(decoded), len(to_match)))
+        # print_msg("match_decoded() list length mismatch (expected {}, got {})".format(len(decoded), len(to_match)))
         return False
     for i in range(len(decoded)):
         if to_match[i] == opcodes.OP_PUSHDATA4 and decoded[i][0] <= opcodes.OP_PUSHDATA4 and decoded[i][0]>0:
             continue  # Opcodes below OP_PUSHDATA4 all just push data onto stack, and are equivalent.
         if to_match[i] != decoded[i][0]:
-            print_msg("match_decoded() unexpected opcode (expected {}, got {})".format(to_match[i], decoded[i][0]))
+            # print_msg("match_decoded() unexpected opcode (expected {}, got {})".format(to_match[i], decoded[i][0]))
             return False
-    print_msg("match_decoded() opcodes match")
+    # print_msg("match_decoded() opcodes match")
     return True
 
 
@@ -414,7 +414,7 @@ def safe_parse_pubkey(x):
 def parse_scriptSig(d, _bytes):
     try:
         decoded = [ x for x in script_GetOp(_bytes) ]
-        print_msg("parse_scriptSig() decoded:", decoded)
+        # print_msg("parse_scriptSig() decoded:", decoded)
     except Exception as e:
         # coinbase transactions raise an exception
         print_error("cannot find address in input script", bh2u(_bytes))
@@ -520,13 +520,13 @@ def get_address_from_output_script(_bytes):
 def parse_input(vds):
     d = {}
     prevout_hash = hash_encode(vds.read_bytes(32))
-    print_msg("parse_input() prevout_hash:", prevout_hash)
+    # print_msg("parse_input() prevout_hash:", prevout_hash)
     prevout_n = vds.read_uint32()
-    print_msg("parse_input() prevout_n:", prevout_n)
+    # print_msg("parse_input() prevout_n:", prevout_n)
     scriptSig = vds.read_bytes(vds.read_compact_size())
-    print_msg("parse_input() scriptSig:", scriptSig)
+    # print_msg("parse_input() scriptSig:", scriptSig)
     sequence = vds.read_uint32()
-    print_msg("parse_input() sequence:", sequence)
+    # print_msg("parse_input() sequence:", sequence)
     d['prevout_hash'] = prevout_hash
     d['prevout_n'] = prevout_n
     d['sequence'] = sequence
@@ -546,7 +546,7 @@ def parse_input(vds):
         else:
             d['scriptSig'] = ''
 
-    print_msg("parse_input() decoded::", d)
+    # print_msg("parse_input() decoded::", d)
 
     return d
 
@@ -590,25 +590,25 @@ def deserialize(raw):
     d = {}
     start = vds.read_cursor
     d['version'] = vds.read_int32()
-    print_msg("deserialize() version:", d['version'])
+    # print_msg("deserialize() version:", d['version'])
     d['time'] = vds.read_uint32()
-    print_msg("deserialize() time:", d['time'])
+    # print_msg("deserialize() time:", d['time'])
     n_vin = vds.read_compact_size()
-    print_msg("deserialize() n_vin:", n_vin)
+    # print_msg("deserialize() n_vin:", n_vin)
     is_segwit = (n_vin == 0)
     if is_segwit:
-        print_msg("deserialize() is_segwit")
+        # print_msg("deserialize() is_segwit")
         marker = vds.read_bytes(1)
-        print_msg("deserialize() is_segwit marker:", marker)
+        # print_msg("deserialize() is_segwit marker:", marker)
         assert marker == b'\x01'
         n_vin = vds.read_compact_size()
-        print_msg("deserialize() n_vin:", n_vin)
+        # print_msg("deserialize() n_vin:", n_vin)
     d['inputs'] = [parse_input(vds) for i in range(n_vin)]
-    print_msg("deserialize() inputs:", d['inputs'])
+    # print_msg("deserialize() inputs:", d['inputs'])
     n_vout = vds.read_compact_size()
-    print_msg("deserialize() n_vout:", n_vin)
+    # print_msg("deserialize() n_vout:", n_vin)
     d['outputs'] = [parse_output(vds, i) for i in range(n_vout)]
-    print_msg("deserialize() outputs:", d['outputs'])
+    # print_msg("deserialize() outputs:", d['outputs'])
     if is_segwit:
         for i in range(n_vin):
             txin = d['inputs'][i]
@@ -622,7 +622,7 @@ def deserialize(raw):
                     txin['type'] = 'p2wsh'
                     txin['address'] = bitcoin.script_to_p2wsh(txin['witnessScript'])
     d['lockTime'] = vds.read_uint32()
-    print_msg("deserialize() lockTime:", d['lockTime'])
+    # print_msg("deserialize() lockTime:", d['lockTime'])
     return d
 
 
